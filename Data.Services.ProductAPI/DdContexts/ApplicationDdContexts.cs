87149686f21dab3aa50bf99c7f17a78d238cada2
aminov <alami@tshtt.uz>
2021-10-19 17:46:24 +0500

    ch02-fin: Db&table w.created

---
+++ Data.Services.ProductAPI/DdContexts/ApplicationDdContexts.cs
@@ -0,0 +1,14 @@
+﻿using Data.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Services.ProductAPI.DdContexts
+{
+    public class ApplicationDdContexts : DbContext
+    {
+        public ApplicationDdContexts(DbContextOptions<ApplicationDdContexts> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
