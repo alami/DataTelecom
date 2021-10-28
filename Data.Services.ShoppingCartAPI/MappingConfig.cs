ad0a5aa013bcddf962b920f93986dbc1a7f258ca
aminov <alami@tshtt.uz>
2021-10-28 18:38:38 +0500

    ch07-04 ShoppingCartAPI Program.cs 4 distribution

---
+++ Data.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace Data.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<ProductDto, Product>();
+                //config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+
+    }
+}
