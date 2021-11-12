8f9d656a6a5cb5d048104b27fa9ebae57beedaee
aminov <alami@tshtt.uz>
2021-11-12 07:52:50 +0500

    ch10 coupon migration

---
+++ Data.Services.CouponAPI/Migrations/20211111141031_AddCouponToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Data.Services.CouponAPI.Migrations
+{
+    public partial class AddCouponToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    CouponId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CouponCode = table.Column<string>(type: "text", nullable: false),
+                    DiscountAmount = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.CouponId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
---
+++ Data.Services.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using Data.Services.CouponAPI.Models.Dto;
+
+namespace Data.Services.CouponAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCouponByCode(string couponCode);
+    }
+}
